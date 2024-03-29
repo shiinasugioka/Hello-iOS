9bdf32a2860c1a4b31db3487fbd38269bc98b535
Shiina Sugioka <shiina12@uw.edu>
2024-03-29 13:38:35 -0700

    created new project

---
+++ HelloiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloiOS
+//
+//  Created by stlp on 3/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
